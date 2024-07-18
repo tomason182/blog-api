2ac2fc116576dee9865dc25a206c8c816ab36e83
tomason182 <elotro182@hotmail.com>
2024-07-18 16:44:03 -0300

    fix: add published status to post schema

--- models/post.js
+++ models/post.js
@@ -6,6 +6,12 @@ const PostSchema = new Schema(
     title: { type: String, required: true },
     author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
     text: { type: text, required: true },
+    status: {
+      type: String,
+      required: true,
+      enum: ["published", "unpublished"],
+      default: "unpublished",
+    },
   },
   {
     timestamps: true,
