2fea688da683bee5c906448e8fc66d7cc882cc5c
tomason182 <elotro182@hotmail.com>
2024-07-20 16:39:27 -0300

    add create user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const crypto = require("node:crypto");
 
 // @desc    Get all users
 // @route   GET /api/users
@@ -18,9 +20,62 @@ exports.users_one_get = asyncHandler(async (req, res) => {
 // @desc    Create a users
 // @route   POST /api/users/
 // @access  Private
-exports.users_create_post = asyncHandler(async (req, res) => {
-  res.json({ msg: "NOT IMPLEMENTED: Create a user" });
-});
+exports.users_create_post = [
+  body("name")
+    .trim()
+    .escape()
+    .isLength({ min: 3, max: 50 })
+    .withMessage("name must be 3 to 50 characters"),
+  body("email").trim().escape().isEmail().withMessage("Not a valid email"),
+  body("password")
+    .trim()
+    .escape()
+    .length({ min: 12 })
+    .withMessage("password should contain at lease 12 characters"),
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    console.log(errors.isEmpty());
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const userExists = await User.findOne({ email: req.body.email });
+
+    if (userExists !== null) {
+      return res.status(400).json([{ message: "User already exists" }]);
+    }
+
+    const salt = crypto.randomBytes(32).toString("hex");
+    crypto.pbkdf2(
+      req.body.password,
+      salt,
+      100000,
+      64,
+      "sha512",
+      async (err, hashedPassword) => {
+        if (err) {
+          return next(err);
+        }
+
+        const user = new User({
+          name: req.body.name,
+          email: req.body.email,
+          hashedPassword: hashedPassword,
+          salt: salt,
+        });
+
+        try {
+          await user.save();
+          return res.status(200).json({ message: "User created successfully" });
+        } catch (err) {
+          return next(err);
+        }
+      }
+    );
+  }),
+];
 
 // @desc    Update a user
 // @route   PUT /api/users/:id
