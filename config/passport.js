651d85a21dfebc0aa170bcabbd0a3984f7c257e8
tomason182 <elotro182@hotmail.com>
2024-07-22 19:03:51 -0300

    fix: redifine passportJWT configuration

--- config/passport.js
+++ config/passport.js
@@ -7,18 +7,20 @@ const opt = {};
 opt.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opt.secretOrKey = process.env.JWT_SECRET;
 
-exports.passport.use(
-  new Strategy(opt, function (payload, done) {
-    User.findOne({ id: payload.sub }, function (err, user) {
-      if (err) {
-        return done(err, false);
-      }
+const strategy = new Strategy(opt, function (payload, done) {
+  User.findOne({ id: payload.sub }, function (err, user) {
+    if (err) {
+      return done(err, false);
+    }
 
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    });
-  })
-);
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  });
+});
+
+module.exports = (passport) => {
+  passport.use(strategy);
+};
